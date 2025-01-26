5debad9dd9ba87cfca6a8b5f4832e32ee862b383
Muniz <rodrigotmuniz@gmail.com>
2025-01-26 07:11:58 -0300

    improve docker-compose

--- api-gateway/src/api-gateway.module.ts
+++ api-gateway/src/api-gateway.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ApiGatewayController } from './api-gateway.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ApiGatewayController } from './api-gateway.controller';
 
 @Module({
   imports: [
@@ -9,8 +9,8 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         name: process.env.PRODUCER_SERVICE_CLIENT || 'PRODUCER_SERVICE_CLIENT',
         transport: Transport.TCP, // TCP communication
         options: {
-          host: process.env.PRODUCERS_PORT ?? 'localhost',
-          port: Number(process.env.PRODUCERS_PORT || 3003),
+          host: process.env.PRODUCER_HOST ?? 'localhost',
+          port: Number(process.env.PRODUCER_PORT || 3003),
         }, // Microservice address
       },
     ]),
